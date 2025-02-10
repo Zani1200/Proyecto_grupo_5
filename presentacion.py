301188041f66b6bb2cd1beeb7d46fa7f75fec633
allave-rep <allave@gmail.com>
2025-02-10 17:35:32 +0100

    .

---
+++ presentacion.py
@@ -0,0 +1,24 @@
+import os
+class presentacion:
+
+    def __init__(self):
+        pass  # Constructor, puedes inicializar variables aquí si es necesario
+
+    def printEncabezado(self):
+        encabezado =  ("=======================================\n")
+        encabezado += ("=== Bienvenido a Experiencias Viajeras ===\n")
+        encabezado += ("=======================================\n")
+        print(encabezado)
+        return None
+
+    def printMenu(self):
+        menu = ("\n============ Menú ==============\n")
+        menu += ("1. Lanza lo que quieras a Open IA\n")
+        menu += ("2. Consultar base de datos\n")
+        menu += ("3. Salir (o escribe 'salir')\n")
+        print(menu)
+        return None
+
+    def limpiarConsola(self):
+        os.system('cls')
+        return None
