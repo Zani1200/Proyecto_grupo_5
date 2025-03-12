c78f5f32a5cc68ebbad00243be54c8a6da5da1c3
allave-rep <allave@gmail.com>
2025-03-12 19:15:14 +0100

    Se borra directorio duplicado Proyecto_grupo_5
    Se añadió por error

--- Proyecto_grupo_5/streamlit_ui/handler.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-def cambiar_pagina(pagina):
-    st.session_state.pagina = pagina
--- Proyecto_grupo_5/streamlit_ui/home.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def mostrar_home():
-    st.title("🌍 Bienvenido a Experiencias Viajeras")
-    st.write("""
-    ¡Planifica tus viajes de manera inteligente y personalizada!
-    Utiliza las opciones del menú para comenzar.
-    """)
