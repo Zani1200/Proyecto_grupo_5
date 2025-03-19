f62ff0ccd3cdb50c61b9f1bb638ed89a2c103b50
allave-rep <allave@gmail.com>
2025-03-19 20:15:00 +0100

    Mejora de legibilidad de listado de interacciones con IA

--- streamlit_ui/ver_base_datos.py
+++ streamlit_ui/ver_base_datos.py
@@ -2,7 +2,7 @@ import streamlit as st
 from database.base_datos_Supabase import BaseDatos
 
 def mostrar_base_datos():
-    st.title("📂 Ver Base de Datos")
+    st.title("📂 Consulta de interacciones con la IA")
 
     # Instanciar la base de datos
     try:
@@ -10,8 +10,17 @@ def mostrar_base_datos():
         interacciones = base_datos.extraer_interacciones_AI()
 
         if interacciones:
-            st.write("Interacciones almacenadas:")
-            st.json(interacciones)
+            st.write("Listado de interacciones almacenadas:")
+            # st.json(interacciones)
+
+            # Se utiliza expander para mostrar textos largos de forma amigable
+            # Invertir la lista para mostrar los más recientes primero
+            for i, interaccion in enumerate(interacciones[::-1]):
+                with st.expander(f"🆕 Interacción {len(interacciones) - i}"):  # Ajusta el número de la interacción
+                    for clave, valor in interaccion.items():
+                        st.markdown(f"**{clave}:** {valor}")
+                    st.markdown("---")  # Separador visual
+
         else:
             st.info("ℹ️ No hay interacciones almacenadas en la base de datos.")
     except Exception as e:
